0625684a8b0721b798eb0aae2c0f4bace099c8c7
Resilienza Industria <resilienzaadm@gmail.com>
2025-05-29 09:52:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -87,3 +87,8 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # usa a porta que o Render fornece
+    app.run(host="0.0.0.0", port=port)
